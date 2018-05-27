0e1b22a34a2e262d5ad9648b717031eac14431ec
Seloris <daniel.djordjevic@viacesi.fr>
2018-05-28 01:32:43 +0200

    build

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,9 +6,23 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
-  goPage1() {
-    this.router.navigate(['page', 1]);
+export class HomeComponent implements OnInit {
+  pageIndexList: number[] = [];
+  isLoading = true;
+
+  constructor(private router: Router) {
+    const max = 10;
+    for (let i = 1; i <= max; i++) {
+      this.pageIndexList.push(i);
+    }
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
+  goPage(i: number) {
+    this.router.navigate(['page', i]);
   }
 }
